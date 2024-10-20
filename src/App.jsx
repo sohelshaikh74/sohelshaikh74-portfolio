2b34300de2df391c5abc7935825b412449666e26
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-10-20 14:50:38 +0530

    All updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 const App = () => {
   return (
     <>
+      {/* <Navbar /> */}
       <Hero />
       <Projects />
       <Skills />
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        iste eaque laboriosam ut ipsa accusamus quod dolore tenetur numquam
+        officiis, autem tempore, ab incidunt dolores corrupti nulla dolor non
+      </p>
+    </div>
+  );
+};
+
+export default Navbar;
